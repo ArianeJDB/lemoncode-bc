f2aea632872f496bd6f558a5ca7332e5511fa4d8
Kairosds <ariane.jurado@gmail.com>
2020-09-11 18:44:20 +0200

    module 7

--- modulo 6 II/randomPick.js
+++ modulo 6 II/randomPick.js
@@ -24,3 +24,56 @@ function isRepeated(random) {
     return sameNumber;
 }
 randomPick(9, 1, 10)
+
+
+const arr =  [2, 1, 3, 5, 3, 2]
+const s = "abacabad"
+
+function firstDuplicate(arr) {
+    let stored = {};
+
+    for (let i = 0; i < arr.length; i++) {
+      if (stored[arr[i]]) {
+        return arr[i]
+      } else {
+       stored[arr[i]] = arr[i]
+      }
+    }
+    return -1;
+}
+
+function firstNotRepeatingCharacter(s) {
+
+    for (let i = 0; i < s.length; i++) {
+     if(s.indexOf(s.charAt(i)) === s.lastIndexOf(s.charAt(i))) {
+         return s.charAt(i)
+         break;
+     }
+    }
+    return '_';
+}
+
+const a = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+     [[7,4,1], 
+     [8,5,2], 
+     [9,6,3]]
+
+function rotateImage(a) {
+    const first = a[0].splice(0,1)
+    a[0].push(first[0])
+    const sec = a[0].splice(1,1)
+    // for (const sub of a) {
+    //     const f = sub.splice(0,1)
+    //         console.log('f', f)
+    //     for (const num of sub) {
+            
+    //     }
+    // }
+}
+   rotateImage(a) 
+console.log('ppp', firstNotRepeatingCharacter(s))
+
+console.log('ppp', firstDuplicate(arr))
---
+++ modulo 7/main.js
@@ -0,0 +1,28 @@
+const regex = /^ES\d{2}(\ ?\d{4}){5}$/
+const pais = /^ES/
+const control = /\d{2}/
+const iban = 'ES6600190020961234567890'
+const ibanSpaces = 'ES66 0019 0020 9612 3456 7890'
+console.log(regex.test(ibanSpaces))
+console.log(regex.test(iban))
+console.log('pais', pais.exec(iban))
+console.log('control', control.exec(iban))
+console.log('pais', pais.exec(ibanSpaces))
+console.log('control', control.exec(ibanSpaces))
+
+
+//matriculas
+
+const regexCar = /^\d{4}\ ?|-?[A-Z]{3}$/i
+const num = /^\d{4}/
+const letras = /[A-Z]{3}$/i
+
+const matriculas = ['2021 GMD', '2345-GMD', '4532BDB', '0320-AAA']
+
+matriculas.forEach(item => {
+  console.log(regexCar.test(item))
+  console.log('numeros', num.exec(item))
+  console.log('letras', letras.exec(item))
+
+
+})
